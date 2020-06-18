c18d1ba88c52ec84f1ab757e1b95f152d7330be3
沈林强 <shenlinqiang_52702@wdai.com>
2020-06-18 14:41:11 +0800

    buffer

--- src/main/java/com/shenlinqiang/mytailbased/client/HttpDownloadHandler.java
+++ src/main/java/com/shenlinqiang/mytailbased/client/HttpDownloadHandler.java
@@ -1,11 +1,20 @@
 package com.shenlinqiang.mytailbased.client;
 
+import com.shenlinqiang.mytailbased.Constants;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.HttpContent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpDownloadHandler extends ChannelInboundHandlerAdapter {
 
@@ -14,25 +23,24 @@ public class HttpDownloadHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         try {
+
+            ByteBufAllocator alloc = ctx.channel().alloc();
+            ByteBuf byteBuf = alloc.directBuffer(Constants.DOWNLOAD_SIZE);
+
             HttpContent content = (HttpContent) msg;
             ByteBuf buf = content.content();
-            while (buf.isReadable()) {
-                String span = buildSpan(buf);
-                if (span == null) {
-                    break;
-                }
-//                LOGGER.info("读入的span:    " + span);
-            }
-            LOGGER.info("已经全部读入");
-            if (lineEnd) {
-                ReadDataTask.startOffset += new Long(buf.capacity());
-            } else {
-                ReadDataTask.startOffset += (long) buf.capacity() - p;
-            }
-            LOGGER.info("下一个开始的偏移量：" + ReadDataTask.startOffset);
-            buf.release();
+            ByteBuffer directBuffer = ByteBuffer.allocateDirect(Constants.DOWNLOAD_SIZE + 1);
 
-            ReadDataTask.semaphore.release();
+//            for (int i = 0; i < buf.capacity(); i++) {
+//                byte aByte = buf.getByte(i);
+//                directBuffer.put(aByte);
+//            }
+            long s = System.currentTimeMillis();
+            buildSpan(byteBuf);
+            buf.release();
+            long e = System.currentTimeMillis();
+            System.out.println("read time: " + (e - s) + "ms");
+//            ReadDataTask.semaphore.release();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -54,27 +62,50 @@ public class HttpDownloadHandler extends ChannelInboundHandlerAdapter {
 
     private static final byte LF = '\n';
     private static byte[] bytes = new byte[1024];
+    private static byte[] transForBuffer = new byte[32 * 1024];
+    private static byte[] tempBuffer = new byte[1024];
     private static int p = 0;
     private static boolean lineEnd = false;
+    private static int i = 0;
 
-    private String buildSpan(ByteBuf buf) {
-        byte readByte = buf.readByte();
+    private void buildSpan(ByteBuf buf) {
         p = 0;
-        while (buf.isReadable()) {
+
+        while (i < buf.capacity()) {
+            byte readByte = buf.getByte(i++);
+//            buf.readBytes(transForBuffer);
             if (LF == readByte) {
-                lineEnd = true;
-                break;
             }
-            bytes[p++] = readByte;
-            readByte = buf.readByte();
-        }
-        if (lineEnd) {
-            return null;
-//            return new String(bytes, 0, p);
-        } else {
-            lineEnd = false;
-            return null;
         }
 
     }
+
+    public static Map<Integer, Batch> ALLDATA = new ConcurrentHashMap<>();
+    public static List<String> temp = new ArrayList<>();
+
+
+    private String readBuffer() {
+        int i = 0;
+        int traceNumber = 0;
+        int batchNo = 0;
+        while (i < transForBuffer.length && LF != transForBuffer[i]) {
+
+        }
+        traceNumber++;
+        if (traceNumber % Constants.BATCH_SIZE == 0) {
+//            ALLDATA.put(batchNo, )
+        }
+        return null;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/shenlinqiang/mytailbased/TestDirectMemory.java
+++ src/test/java/com/shenlinqiang/mytailbased/TestDirectMemory.java
@@ -1,8 +1,49 @@
 package com.shenlinqiang.mytailbased;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class TestDirectMemory {
+    private static final byte LF = '\n';
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fin = new FileInputStream(new File("/Users/jerryq/Desktop/bisai/trace1.data"));
+        FileChannel channel = fin.getChannel();
 
+
+        ByteBuffer direct = ByteBuffer.allocateDirect(Constants.DOWNLOAD_SIZE);
+        channel.read(direct);
+        long s = System.currentTimeMillis();
+        readDirectBuffer(direct);
+//            direct.get(bytes);
+//            readHeapBuffer(bytes);
+        long e = System.currentTimeMillis();
+        System.out.println("花费时间： " + (e - s) + "ms");
+    }
+
+    public static void readDirectBuffer(ByteBuffer byteBuffer) {
+        int i = 0;
+        byte[] bytes = new byte[1024];
+        int p = 0;
+        while (i < byteBuffer.capacity()) {
+            byte b = byteBuffer.get(i);
+            bytes[p++] = b;
+            if (LF == b) {
+//                System.out.println(new String(bytes, 0, p));
+                p = 0;
+//                System.out.println("find LF,i=" + i);
+            }
+            i++;
+        }
+    }
+
+    public static void readHeapBuffer(byte[] bytes) {
+        int i = 0;
+        while (i < bytes.length) {
+            byte b = bytes[i];
+            i++;
+        }
     }
 }
